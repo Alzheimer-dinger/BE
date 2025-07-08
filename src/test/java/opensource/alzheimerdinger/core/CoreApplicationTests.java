1b38b09ec4e195609e3cac159254f67c59c58629
koungq <guu119@naver.com>
2025-07-08 15:25:21 +0900

    init

---
+++ src/test/java/opensource/alzheimerdinger/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package opensource.alzheimerdinger.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
