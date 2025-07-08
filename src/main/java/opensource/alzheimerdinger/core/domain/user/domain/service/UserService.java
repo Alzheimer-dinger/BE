36ddfc7ae0e2ce44f178811ca619659434fd23a3
koungq <guu119@naver.com>
2025-07-09 00:39:02 +0900

    feat: 인증 인가 로직

--- src/main/java/opensource/alzheimerdinger/core/domain/user/domain/service/UserService.java
+++ src/main/java/opensource/alzheimerdinger/core/domain/user/domain/service/UserService.java
@@ -1,16 +1,27 @@
 package opensource.alzheimerdinger.core.domain.user.domain.service;
 
 import lombok.RequiredArgsConstructor;
-import opensource.alzheimerdinger.core.domain.user.application.request.SignUpRequest;
+import opensource.alzheimerdinger.core.domain.user.application.dto.request.LoginRequest;
+import opensource.alzheimerdinger.core.domain.user.application.dto.request.SignUpRequest;
 import opensource.alzheimerdinger.core.domain.user.domain.entity.User;
 import opensource.alzheimerdinger.core.domain.user.domain.repository.UserRepository;
+import opensource.alzheimerdinger.core.global.exception.RestApiException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static opensource.alzheimerdinger.core.global.exception.code.status.GlobalErrorStatus._NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RestApiException(_NOT_FOUND));
+    }
 
     public boolean isAlreadyRegistered(String email) {
         return userRepository.existsByEmail(email);
@@ -20,8 +31,9 @@ public class UserService {
         userRepository.save(
                 User.builder()
                         .email(request.email())
-                        .password()
+                        .password(passwordEncoder.encode(request.password()))
                         .role(request.role())
+                        .build()
         );
     }
 }
