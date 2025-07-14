d9f8cb39df97645ba68c35d7a3b70c2109334f85
JANGWOO JUNG <81564742+Smallt0wn@users.noreply.github.com>
2025-07-14 16:19:03 +0900

    feat: batch 템플릿 작성

--- src/main/java/opensource/alzheimerdinger/core/domain/batch/application/usecase/XXXUseCase.java
+++ src/main/java/opensource/alzheimerdinger/core/domain/batch/application/usecase/XXXUseCase.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import opensource.alzheimerdinger.core.domain.batch.application.dto.request.XXXBatchRequest;
 import opensource.alzheimerdinger.core.domain.batch.application.dto.response.XXXBatchResponse;
-import opensource.alzheimerdinger.core.domain.batch.domain.service.XXXService;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -20,7 +19,6 @@ public class XXXUseCase {
 
     private final JobLauncher jobLauncher;
     private final Job xxxJob;
-    private final XXXService xxxService;
 
     public void executeXXXBatch(String date) {
         try {
